edbec4ad8514689c2424717ec9a7b3e0ca3393ba
Evanesce <anthoniusevan@gmail.com>
2024-05-02 17:40:32 +0700

    minor fixes

--- FlightReservationProject/FlightReservationProject/BoardingPass.cs
+++ FlightReservationProject/FlightReservationProject/BoardingPass.cs
@@ -125,31 +125,57 @@ namespace FlightReservationProject
         }
         private int GenerateId(AES aes)
         {
-            string sql = "SELECT COUNT(id) FROM boarding_pass WHERE flight_number = '" + aes.Encrypt(FlightNumber) + "'";
+            string sql = "SELECT COUNT(id) FROM boarding_pass WHERE flight_number = @flightNum";
 
-            MySql.Data.MySqlClient.MySqlDataReader results = dbConnection.ExecuteQuery(sql);
-            int count = 0;
-            while (results.Read())
+            using (MySqlConnection connection = new MySqlConnection(dbConnection.GetConnectionString()))
             {
-                count = results.GetInt32(0);
-            }
+                using (MySqlCommand com = new MySqlCommand(sql, connection))
+                {
+                    com.Parameters.AddWithValue("@flightNum", aes.Encrypt(FlightNumber));
+                    connection.Open();
+                    using (MySqlDataReader results = com.ExecuteReader())
+                    {
+                        int count = 0;
+                        while (results.Read())
+                        {
+                            count = results.GetInt32(0);
+                        }
 
-            return count + 1;
+                        return count + 1;
+                    }
+                }
+            }     
         }
         private void CreateBoardingPass(AES aes)
         {
-            string sql = string.Format("INSERT INTO boarding_pass(id, class_id, gate, flight_number, seat_number, passenger_id) VALUES('{0}','{1}','{2}','{3}','{4}','{5}')", GenerateId(aes), FlightClass.Id, aes.Encrypt(Gate), aes.Encrypt(FlightNumber), aes.Encrypt(Seat), Passenger.Id);
-            dbConnection.ExecuteNonQuery(sql);
+            string sql = "INSERT INTO boarding_pass(id, class_id, gate, flight_number, seat_number, passenger_id) VALUES(@id, @class,@gate,@flightNum,@seat,@pId)";
+            using (MySqlConnection connection = new MySqlConnection(dbConnection.GetConnectionString()))
+            {
+                using (MySqlCommand com = new MySqlCommand(sql, connection))
+                {
+                    com.Parameters.AddWithValue("@id", GenerateId(aes));
+                    com.Parameters.AddWithValue("@class", FlightClass.Id);
+                    com.Parameters.AddWithValue("@gate", aes.Encrypt(Gate));
+                    com.Parameters.AddWithValue("@flightNum", aes.Encrypt(FlightNumber));
+                    com.Parameters.AddWithValue("@seat", aes.Encrypt(Seat));
+                    com.Parameters.AddWithValue("@pId", Passenger.Id);
+
+                    connection.Open();
+                    com.ExecuteNonQuery();
+                }
+            }
         }
         public static BoardingPass GetBoardingPass(Passenger p, string flightNumber, AES aes)
         {
-            string sql = "SELECT b.id, b.class_id, c.name, b.gate, b.flight_number, b.seat_number, b.passenger_id FROM boarding_pass b INNER JOIN class c ON b.class_id = c.id WHERE b.passenger_id = '" + p.Id + "' AND b.flight_number = '" + aes.Encrypt(flightNumber) +"'";
+            string sql = "SELECT b.id, b.class_id, c.name, b.gate, b.flight_number, b.seat_number, b.passenger_id FROM boarding_pass b INNER JOIN class c ON b.class_id = c.id WHERE b.passenger_id = @id AND b.flight_number = @flightNum";
 
             BoardingPass pass=null;
             using (MySqlConnection connection = new MySqlConnection(dbConnection.GetConnectionString()))
             {
                 using (MySqlCommand cmd = new MySqlCommand(sql, connection))
                 {
+                    cmd.Parameters.AddWithValue("@id", p.Id);
+                    cmd.Parameters.AddWithValue("@flightNum", aes.Encrypt(flightNumber));
                     connection.Open();
                     using (MySqlDataReader results = cmd.ExecuteReader())
                     {
